4209cb2df6631de1680887f1a2d51609005dd95b
Sabeeh Ajibade <detunjinov@gmail.com>
2022-07-19 05:11:58 +0100

    backup reinstall

--- backend/routes/product.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const { createProduct } = require('../controllers/productController')
-
-
-router.post('/create', createProduct)
-
-module.exports = router
